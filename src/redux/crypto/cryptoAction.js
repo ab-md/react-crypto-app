9b910463c6f92b6b574b9de6f50bea5c8b90c780
Abolfazl Madadi <abmdofficial@gmail.com>
2022-01-26 13:30:29 +0330

    some changes in api url

--- src/redux/crypto/cryptoAction.js
+++ src/redux/crypto/cryptoAction.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const cryptoAction = () => (dispatch) => {
 
-    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false";
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false";
 
     dispatch ({
         type: "GET_CRYPTO_REQUEST"
@@ -11,21 +11,6 @@ const cryptoAction = () => (dispatch) => {
     axios.get(url)
         .then(response => dispatch({type: "GET_CRYPTO_SUCCESS", payload: response.data}))
         .catch(error => dispatch({type: "GET_CRYPTO_FAILURE", payload: error}))
-
-    // const getData = async () => {
-    //    try {
-    //     const response = await axios.get(url);
-    //     return {
-    //         type: "GET_CRYPTO_SUCCESS",
-    //         payload: response.data
-    //     }
-    //    } catch (error) {
-    //        return {
-    //            type: "GET_CRYPTO_FAILURE",
-    //            payload: error
-    //        }
-    //    }
-    // }
 }
 
 export default cryptoAction;
